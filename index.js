c7d68b6b2958c30706f400474c1958b56812e350
RUFU4 <iivangod007@mail.ru>
2024-09-06 16:02:12 +0300

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 let str = "Akbewka" //Для второй задачи
-let arr = [cat, dog, parrot, horse] //Для третьей задачи
+let arr = ["cat", "dog", "parrot", "horse"] //Для третьей задачи
+let arr2 = ["cat", "dog", "parrot", "horse", "fish", "chicken", "lion"] //Для четвертой задачи
+let arr3 = [31, 10, 'chicken', 9, 'fish', 10] //Для пятой задачи
+let obj =  {
+    firstName: 'Jack',
+    lastName: 'Sparrow',
+    age: 25,
+    location: 'Caribbean sea',
+  }
 
 // Первая задачка
 console.log(str[3])
@@ -29,6 +37,7 @@ function optionThree(){
         else accum.push(item)
         return accum
     }, [])
+    console.log(str4)
 }
 optionOne()
 optionTwo()
@@ -36,4 +45,32 @@ optionThree()
 
 //Третья задачка
 console.log(arr.findIndex((item) => item === "parrot"))
-arr
+
+function MyFindIndexOne(element, arr){
+    let output = arr.reduce((acc, item, index) => {
+        if (element === item){
+            acc = index
+        }
+        return acc
+    }, 0)
+    return output
+}
+console.log(MyFindIndexOne("parrot", arr))
+
+//четвертая задачка 
+
+delete arr2[4]
+arr2.shift()
+arr2.splice(arr2.length - 2, 2)
+
+//пятая задача 
+
+let strs = arr3.filter((item) => typeof item === "string")
+console.log(strs)
+let tens = arr3.filter((item) => item === 10)
+console.log(tens)
+
+//шестая 
+for (let key in obj){
+    console.log(key + " : " + obj[key])
+}
